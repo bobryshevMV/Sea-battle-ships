c9ac695221d653dedb0438f0673de92cc53a481c
bobryshev_m.v@mail.ru <bobryshev_m.v@mail.ru>
2018-12-08 13:22:03 +0400

    maket V1.1

--- Sea-battle-ships/Views/FormMain.Designer.cs
+++ Sea-battle-ships/Views/FormMain.Designer.cs
@@ -54,12 +54,7 @@
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
             this.menuStrip1.SuspendLayout();
-            this.SeaOwnPanel.SuspendLayout();
             this.SeaOwnGroupBox.SuspendLayout();
             this.SeaEnemyGroupBox.SuspendLayout();
             this.SuspendLayout();
@@ -106,10 +101,6 @@
             // SeaOwnPanel
             // 
             this.SeaOwnPanel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.SeaOwnPanel.Controls.Add(this.button4);
-            this.SeaOwnPanel.Controls.Add(this.button3);
-            this.SeaOwnPanel.Controls.Add(this.button2);
-            this.SeaOwnPanel.Controls.Add(this.button1);
             this.SeaOwnPanel.Location = new System.Drawing.Point(20, 50);
             this.SeaOwnPanel.Name = "SeaOwnPanel";
             this.SeaOwnPanel.Size = new System.Drawing.Size(220, 220);
@@ -309,41 +300,6 @@
             this.label4.TabIndex = 7;
             this.label4.Text = "4 палубы";
             // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(194, 197);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(20, 20);
-            this.button1.TabIndex = 0;
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(194, 178);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(20, 20);
-            this.button2.TabIndex = 1;
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
-            // 
-            // button3
-            // 
-            this.button3.Location = new System.Drawing.Point(194, 159);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(20, 20);
-            this.button3.TabIndex = 2;
-            this.button3.UseVisualStyleBackColor = true;
-            this.button3.Click += new System.EventHandler(this.button3_Click);
-            // 
-            // button4
-            // 
-            this.button4.Location = new System.Drawing.Point(194, 140);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(20, 20);
-            this.button4.TabIndex = 3;
-            this.button4.UseVisualStyleBackColor = true;
-            // 
             // FormMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -361,7 +317,6 @@
             this.Text = "SeaBattleShips";
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
-            this.SeaOwnPanel.ResumeLayout(false);
             this.SeaOwnGroupBox.ResumeLayout(false);
             this.SeaOwnGroupBox.PerformLayout();
             this.SeaEnemyGroupBox.ResumeLayout(false);
@@ -399,10 +354,6 @@
         private System.Windows.Forms.TextBox textBox7;
         private System.Windows.Forms.TextBox textBox6;
         private System.Windows.Forms.TextBox textBox5;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Button button4;
     }
 }
 
